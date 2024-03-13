2853455a8c37af93c8af77dc14db22afc5dbfb02
Kim Nolle <nollekim@yahoo.de>
2024-03-13 22:21:22 +0000

    Create CSV files with n-gram data.
    TODOs: add filters to reduce dimenstionality

--- counts.py
+++ counts.py
@@ -25,23 +25,39 @@ import json
 
 
 input_directory = './data/tokenized-txt'
-filename = '2018_UBC_Strategic_Plan_Full-20180425.txt'
+output_directory = './data'
 
-input_path = os.path.join(input_directory, filename)
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
-with open(input_path, 'r', encoding='utf-8') as file:
-    uni_grams = json.load(file)
+# Create CSV with all uni-gram frequency counts and seperate CSV with all bi-gram frequency counts
+df_all_uni = pd.DataFrame()
+df_all_bi = pd.DataFrame()
+for filename in os.listdir(input_directory):
+    if filename.endswith('.txt'):
+        print(f"Counting {filename} ...")
+
+        input_path = os.path.join(input_directory, filename)
+        
+        with open(input_path, 'r', encoding='utf-8') as file:
+            uni_grams = json.load(file)
+            
+        # Generate bi-grams
+        bi_grams = [word1+' '+word2 for word1, word2 in zip(uni_grams[:-1], uni_grams[1:])]
+            
+        # Count frequency of uni-grams
+        df_uni = pd.DataFrame(Counter(uni_grams), index=[0])
+        df_uni["filename"] = filename
+        df_all_uni = pd.concat([df_all_uni, df_uni])
+        
+        # Count frequency of bi-grams
+        df_bi = pd.DataFrame(Counter(bi_grams), index=[0])
+        df_bi["filename"] = filename
+        df_all_bi = pd.concat([df_all_bi, df_bi])
+        
     
-
-# Count and calculate relative frequency of uni-grams
-df_uni = pd.DataFrame(Counter(uni_grams), index=[0])
-df_uni = df_uni.div([len(uni_grams)], axis=0)
-
-# Generate bi-grams
-bi_grams = [word1+' '+word2 for word1, word2 in zip(uni_grams[:-1], uni_grams[1:])]
-
-# Count and calculate relative frequency of bi-grams
-df_bi = pd.DataFrame(Counter(bi_grams), index=[0])
-df_bi = df_bi.div([len(bi_grams)], axis=0)
-
-print(df_bi)
+df_all_uni = df_all_uni.fillna(0)
+df_all_uni.to_csv(os.path.join(output_directory, 'all_uni-grams.csv'), index=False)
+    
+df_all_bi = df_all_bi.fillna(0)
+df_all_bi.to_csv(os.path.join(output_directory, 'all_bi-grams.csv'), index=False)
