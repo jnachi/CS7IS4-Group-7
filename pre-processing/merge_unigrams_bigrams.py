8583730a28a1855c7f11c6f64a63ede1f721b804
Kim Nolle <nollekim@yahoo.de>
2024-04-13 16:02:05 +0100

    Make file names consistent with archive

--- pre-processing/merge_unigrams_bigrams.py
+++ pre-processing/merge_unigrams_bigrams.py
@@ -2,14 +2,14 @@
 
 import pandas as pd
 
-df_uni_grams = pd.read_csv('data/extracted-words/uni-grams_rel-frequ.csv')
-df_bi_grams = pd.read_csv('data/extracted-words/bi-grams_rel-frequ.csv')
+df_uni_grams = pd.read_csv('data/uni-grams_rel-frequ.csv')
+df_bi_grams = pd.read_csv('data/bi-grams_rel-frequ.csv')
 
 
-df_universities = pd.read_csv('Merged_CSV.csv')  
+df_universities = pd.read_csv('university_list_combined.csv')  
 
 merged_df = pd.merge(df_universities, df_uni_grams, on='filename', how='left')
 merged_df = pd.merge(merged_df, df_bi_grams, on='filename', how='left')
 
 merged_df.fillna(0, inplace=True)
-merged_df.to_csv('Merged_Data_with_Frequencies.csv', index=False)
+merged_df.to_csv('features_n-grams_merged.csv', index=False)
